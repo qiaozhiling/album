c56231554b3576b15088039934fcfd022e1bb1de
qiaozhiling <189050097330@163.com>
2022-06-16 11:26:22 +0800

    feat#swagger带令牌

--- src/main/kotlin/com/qiao/album/config/SwaggerConfig.java
+++ src/main/kotlin/com/qiao/album/config/SwaggerConfig.java
@@ -5,12 +5,18 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -19,13 +25,19 @@ public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
+
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        List<Parameter> pars = new ArrayList<>();
+        pars.add(tokenPar.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .enable(enable)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.qiao.album.controller")) //添加需生成文档类所在包路径
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(pars);//注意这里;
     }
 
     private ApiInfo apiInfo() {
